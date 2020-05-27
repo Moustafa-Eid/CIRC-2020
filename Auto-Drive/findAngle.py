c184c9ad13e36e9ec9178816c9aa07a389b16ea6
aimhus <51844146+aimhus@users.noreply.github.com>
2020-05-27 18:24:05 -0400

    Add files via upload

--- Auto-Drive/findAngle.py
+++ Auto-Drive/findAngle.py
@@ -7,45 +7,48 @@ Created on Tue Mar 10 17:27:04 2020
 
 import math
 
-p1 = [12, 4]                                # Destination Point (latitude, longitude)
-p2 = [9, 6]                                 # Dummy second point. I want to create this by taking our ref
-                                            # and findHeading, then extrapolating it to any point on that line
-ref = [0,0]                                 # Reference point, ie. Rover's current location
-    
+ref = (0,0)                                 # Reference point, ie. Rover's current location
+
+unit = (1,0)
+
+
+def vector(a, r) :                      
+    """Returns difference between a point and reference point."""
+    return [a[0] - r[0], a[1] - r[1]]
+
+def length(a) :                        
+    """Returns the length of the vector connecting cartesian point a and the origin (0, 0)."""
+    return math.sqrt(a[0]**2 + a[1]**2) 
+
+def dotprod(a, b) :                    
+    """Returns dot product of two vectors (needed to find angle)."""    
+    return a[0]*b[0] + a[1]*b[1]
+
+def angle(a, b) :                       
+    """Finds angle between rover and destination point in radians."""
+    cosAng = dotprod(a, b)/(length(a)*length(b))
+    return math.acos(cosAng)
+
+def radToDeg(a) :                       
+    """Converts angle from radians to degrees"""
+    return a*180/math.pi
+
 def findAngle(pointOne, pointTwo) :
-    
-    def vector(a, r) :                      # Returns difference between a point and reference point
-        return [a[0] - r[0], a[1] - r[1]]
-    
-    point1 = vector(p1, ref)                # Points in relation to rover's current position. These
-    point2 = vector(p2, ref)                # are the points we work with going forward
-    
-    def length(a) :                         # Accepts inputs for a point and reference point, returns the length
-        return math.sqrt(a[0]**2 + a[1]**2) # Can likely be replaced by findDistance
-    
-    def dotprod(a, b) :                     # Returns dot product of two vectors (needed to find angle)    
-        return a[0]*b[0] + a[1]*b[1]
-    
-    def angle(a, b) :                       # Finds angle between rover and destination point in radians
-        cosAng = dotprod(a, b)/(length(a)*length(b))
-        return math.acos(cosAng)
-    
-    def radToDeg(a) :                       # Converts angle to degrees
-        return a*180/math.pi
-    
+    """Returns the angle between two points using the origin as a reference point."""
+    point1 = vector(pointOne, ref)                
+    point2 = vector(pointTwo, ref)                
+
     ang = radToDeg(angle(point1, point2))
     
-    print("The angle in degrees is " + str(round(ang, 1)))      # Prints the angle to 1 decimal point
-    return ang                                                  # Actual angle accuracy is much higher
+    return ang
 
-print(findAngle(p1, p2))
-
-
-
-
-
-
-# ok so what I'm thinking is to have a point that is our current gps location, 
-# use that as a reference point (assuming it's (0,0) rn). We can use this reference point
-# along with our heading to extrapolate a point somewhere further, then use that and our
-# desired location to calculate angle. Then translate that angle to PWM code to make the wheels move and ta-da    
+def find_angle(current, destination, heading) :
+    """Returns the angle the rover must turn to face its destination point given current heading."""
+    default = (1, 0)
+    parallel_vector = (destination[0] - current[0], destination[1] - current[1])
+    if destination[1] < current[1] :
+        pv_angle = 360 - findAngle(parallel_vector, default)
+    else : 
+        pv_angle = findAngle(parallel_vector, default)
+    turn_angle = pv_angle - heading/10
+    return turn_angle
--- Auto-Drive/main.py
+++ Auto-Drive/main.py
@@ -2,12 +2,97 @@
 """
 Created on Tue Mar 10 18:56:55 2020
 
-@author: aiman
+@author: Aiman Hussaini
+
+Some Details:
+
+This file simulates the rover's movement and prints it to the console.
+It's given its current location (eventually will be taken from the gps module)
+and its heading (read from the compass module).
+Our compass module returns a 4-bit value, a number from 0-3599. Each
+increment is equal to 0.1 degrees, so all angles = heading/10 
+(eg. 1800 = 180 degrees from its original direction when powered on).
+Eventually will read/write the heading to a text file when we have access to the module.
+The program uses simple cartesian points for testing but can easily be
+swapped out for more accurate gps coordinates.
+
+Functions:
+- Prints current location and heading
+- Determines most efficient turn to face destination point
+- Calculates distance to drive
+- Updates location and writes to a text file
+- Basic graph showing the rover's trajectory
 """
 import math
-import findAngle
-import findLocation
-import findHeading
-import findDistance
-import sendArduino
+import findAngle as ang
+import matplotlib.pyplot as plt
 
+f = open("Previous Locations.txt", "w+")
+
+# TODO:
+# Create a grid ui visualization that shows the rover's 
+# movement
+
+class Rover:
+    heading = 0 # (read continuously from file)
+    current_location = (0, 0)
+    current_speed = 0 # Undecided Parameter
+    distance_to_travel = 0 # Undecided Parameter
+    # Add more parameters if necessary
+
+    def __init__(self, loc, heading):
+        self.current_location = loc
+        self.heading = heading
+
+    def move(self, destination) :
+        print("Current Location: " + str(self.current_location))
+        print("Current Heading: " + str(self.heading))
+        a = ang.find_angle(self.current_location, destination, self.heading)
+        # This next block of code just ensures the rover takes the most
+        # efficient turn (eg. turns 90 degrees right instead of 270 degrees left)
+        if abs(a) > 180 :
+            while(a > 360) :
+                a -= 360
+            while(a < -360) :
+                a += 360
+            if -180 > a and a > -360 :
+                a += 360
+            if a > 180 and a < 360 :
+                a = 360 - a
+            angle = a
+        else:
+            angle = a
+        print("Turning " + str(angle) + " degrees")   # Positive angle rotates left, negative rotates right
+        new_heading = self.heading + round(angle*10)
+        if new_heading < 0 : 
+            self.heading = (3600 + new_heading)%3600
+        else :
+            self.heading = new_heading%3600
+        print("Current Heading: " + str(self.heading))
+        print("Moving " + str(ang.length((destination[0]-self.current_location[0], destination[1]-self.current_location[1]))) + " metres")
+        self.current_location = destination
+        print("New Location: " + str(self.current_location) + "\n")
+        xloc.append(self.current_location[0])
+        yloc.append(self.current_location[1])
+
+rover = Rover((12, 9), 1200) # current position and heading
+f.write(str(rover.current_location) + "\n")
+xloc = [rover.current_location[0]]
+yloc = [rover.current_location[1]]
+
+coordinates = [(1, 16), (2, 2), (6, 9), (5, 9), (5, 10)]
+
+
+
+for x in coordinates :
+    rover.move(x)
+    f.write(str(rover.current_location) + "\n")
+
+f.close()
+
+# PLOT/VISUALIZATION STARTS HERE
+
+plt.plot(xloc, yloc)
+plt.plot(xloc, yloc, 'ro')
+plt.axis([0, 25, 0, 25])
+plt.show()
